6d867abb84b99e279fadb3c5cefa0c1563d76e0d
Eman-AbdElZaher <e.m.abdelzaher@gmail.com>
2021-06-01 18:17:16 +0200

    add models

---
+++ src/app/models/Interfaces/IProduct.ts
@@ -0,0 +1,10 @@
+export interface IProduct{
+  id:number;
+  name:string;
+  price:number;
+  description:string,
+  image:string,
+  quantity:number,
+  subCategoryId:number,
+  color:string
+}
---
+++ src/app/models/Interfaces/ISubCategory.ts
@@ -0,0 +1,6 @@
+export interface ISubCategory{
+  id:number;
+  name:string;
+  image:string;
+  categoryId:number;
+}

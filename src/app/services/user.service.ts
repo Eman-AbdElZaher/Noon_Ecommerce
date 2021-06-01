1e4b1cb7f5dcbc46cb6d3f642bdbff502c02058e
nessma hamad <nessmahamad111@gmail.com>
2021-06-02 01:51:10 +0200

    Update

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Iuser } from '../models/iuser';
+import { Iuser } from '../models/Interfaces/iuser';
 
 @Injectable({
   providedIn: 'root'

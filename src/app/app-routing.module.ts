e33ec3d972c10f2b74744cef277cdc65b49d8b68
nessma hamad <nessmahamad111@gmail.com>
2021-06-03 21:07:56 +0200

    Add login link in register form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './components/client/register/register.component';
+import { LoginComponent } from './components/login/login/login.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { UnathorizedPageComponent } from './components/unathorized-page/unathorized-page.component';
 
 const routes: Routes = [
-  {
-    path: 'login',
-    loadChildren: () => import('./components/login/login.module')
-      .then(mod => mod.LoginModule)
-  },
+  // {
+  //   path: 'login',
+  //   loadChildren: () => import('./components/login/login.module')
+  //     .then(mod => mod.LoginModule)
+  // },
+  {path:'login',component:LoginComponent},
+  {path:'register',component:RegisterComponent},
   {path:'unathorized',component:UnathorizedPageComponent},
   { path: '**', component: PageNotFoundComponent }
 ];

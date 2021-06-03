bf20d5c7d51b2905686a30b4b31bd8e339dfa6d9
nessma hamad <nessmahamad111@gmail.com>
2021-06-03 21:14:13 +0200

    note

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { PageNotFoundComponent } from './components/page-not-found/page-not-foun
 import { UnathorizedPageComponent } from './components/unathorized-page/unathorized-page.component';
 
 const routes: Routes = [
-  // {
+  // { انا عملت كومنت ع دى علشان كنت بجرب لينك الlogin اللى ف register page بس متقلقوش كله شغال 
   //   path: 'login',
   //   loadChildren: () => import('./components/login/login.module')
   //     .then(mod => mod.LoginModule)

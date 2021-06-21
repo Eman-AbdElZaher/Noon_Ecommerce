82c5ab4b12de1a9828546377d4b12db9cab52990
Shymaa Ashraf <shymaaashraf876@gmail.com>
2021-06-22 01:58:14 +0200

    Update in admin Panel And add HeaderPanel Coponent

---
+++ src/app/components/sharedComponent/headeradmin/headeradmin.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+declare var $:any;
+@Component({
+  selector: 'app-headeradmin',
+  templateUrl: './headeradmin.component.html',
+  styleUrls: ['./headeradmin.component.scss']
+})
+export class HeaderadminComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
